c0433302ac5f80309c034f2b6427b40c111ff840
Jemma Daniel <134346753+JemmaLDaniel@users.noreply.github.com>
2025-04-17 10:44:56 +0100

    exp: test-run winnow train and predict

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 winnow train --data-source=instanovo --dataset-config-path=configs/train.yaml --model-output-folder=model --dataset-output-path=data/train_output.csv
 
-winnow predict --data-source=instanovo --dataset-config-path=configs/predict.yaml --model-folder=model --dataset-output-path=data/predict_output.csv
+winnow predict --data-source=instanovo --dataset-config-path=configs/predict.yaml --model-folder=model --method=winnow --fdr-threshold=0.05 --confidence-column="calibrated_confidence" --output-path=data/predict_output.csv
