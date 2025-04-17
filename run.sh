6fa83debaa0a955740082d006a8203fe57ac8e60
Jemma Daniel <134346753+JemmaLDaniel@users.noreply.github.com>
2025-04-17 10:30:09 +0100

    exp: test-run winnow train and predict

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 winnow train --data-source=instanovo --dataset-config-path=configs/train.yaml --model-output-folder=model --dataset-output-path=data/train_output.csv
 
-winnow predict --data-source=instanovo --dataset-config-path=configs/predict.yaml --model-input-folder=model --dataset-output-path=data/predict_output.csv
+winnow predict --data-source=instanovo --dataset-config-path=configs/predict.yaml --model-folder=model --dataset-output-path=data/predict_output.csv
