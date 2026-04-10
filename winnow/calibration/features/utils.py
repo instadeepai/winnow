489cd887a1f91a4a840e950ca75ece8ee37925a3
Jemma Daniel <134346753+JemmaLDaniel@users.noreply.github.com>
2026-04-10 15:36:41 +0100

    chore: move xcorr constants into constants file

--- winnow/calibration/features/utils.py
+++ winnow/calibration/features/utils.py
@@ -12,7 +12,14 @@ import numpy as np
 import pandas as pd
 
 from winnow.datasets.calibration_dataset import CalibrationDataset
-from winnow.calibration.features.constants import CARBON_ISOTOPE_MASS_SHIFT
+from winnow.calibration.features.constants import (
+    CARBON_ISOTOPE_MASS_SHIFT,
+    XCORR_BIN_SIZE,
+    XCORR_BIN_OFFSET,
+    XCORR_NUM_WINDOWS,
+    XCORR_MAX_OFFSET,
+    XCORR_WINDOW_NORM_VALUE,
+)
 
 ########################################################
 # Helper functions
@@ -629,17 +636,6 @@ def compute_spectral_angle(
 ########################################################
 # Cross-correlation Score (xcorr)
 ########################################################
-
-# Default bin width matching Comet's fragment_bin_tol for near-unit-dalton bins.
-XCORR_BIN_SIZE = 1.0005079
-# Default bin offset matching Comet's fragment_bin_offset.
-XCORR_BIN_OFFSET = 0.4
-# Number of equal-width m/z windows for intensity normalization (SEQUEST default).
-XCORR_NUM_WINDOWS = 10
-# Maximum m/z offset in bins for background subtraction (SEQUEST default ±75).
-XCORR_MAX_OFFSET = 75
-# Normalization target for the maximum intensity within each window.
-XCORR_WINDOW_NORM_VALUE = 50.0
 
 
 def _bin_observed_spectrum(
